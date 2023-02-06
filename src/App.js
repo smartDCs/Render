8a511dd5a2dc3e38cb6cf612a8a54b7339d8c115
Domotiz Arq <davidchicaisa@gmail.com>
2023-02-06 17:21:39 -0500

    renderizando con three js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component, Suspense } from "react";
 import "./App.css";
+
 import Scene from "./components/Scene.js";
-import { Canvas } from "react-three-fiber";
+import {Canvas } from "react-three-fiber";
 import Casa from "./components/casa/Casa";
-import { OrbitControls,Sky,Stats,Text3D} from "@react-three/drei";
+import { OrbitControls,Sky,Stats} from "@react-three/drei";
 
 function App() {
+ 
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <Canvas camera={{ zoom: 1, position: [15, 20, 15] }}>
@@ -13,7 +15,7 @@ function App() {
         <pointLight position={[35, 35, 0]} intensity={0.7} />
         <pointLight position={[-35, 35, 0]} intensity={0.8} />
 
-
+      
         
         <Sky
         distance={45000}
@@ -21,22 +23,14 @@ function App() {
         inclination={0}
         azimuth={0.25}
         />
-                <Text3D
-        scale={[10, 10, 10]}
-        color="black" // default
-        anchorX="center" // default
-        anchorY="middle" // default
-        position={[0,35,0]}
-      >
-        HELLO WORLD
-      </Text3D>
+       
         <Suspense fallback={null}>
           <OrbitControls  
           enableDamping={true}
           enablePan={true}
           dampingFactor={0.4}
           />
-              <Stats/>   
+             
           <Casa/>
   
        
