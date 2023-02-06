bec2c51f3587cabc60c6283270d8465d729b9a60
Domotiz Arq <davidchicaisa@gmail.com>
2023-02-06 17:04:19 -0500

    renderizando con three js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import Scene from "./components/Scene.js";
 import { Canvas } from "react-three-fiber";
 import Casa from "./components/casa/Casa";
-import { OrbitControls,Sky } from "@react-three/drei";
+import { OrbitControls,Sky,Stats,Text3D} from "@react-three/drei";
+
 function App() {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
@@ -11,16 +12,37 @@ function App() {
         <ambientLight intensity={0.5} />
         <pointLight position={[35, 35, 0]} intensity={0.7} />
         <pointLight position={[-35, 35, 0]} intensity={0.8} />
+
+
+        
         <Sky
-        distance={450000}
+        distance={45000}
         sunPosition={[0,1,0]}
         inclination={0}
         azimuth={0.25}
         />
+                <Text3D
+        scale={[10, 10, 10]}
+        color="black" // default
+        anchorX="center" // default
+        anchorY="middle" // default
+        position={[0,35,0]}
+      >
+        HELLO WORLD
+      </Text3D>
         <Suspense fallback={null}>
-          <OrbitControls />
-          <Casa />
+          <OrbitControls  
+          enableDamping={true}
+          enablePan={true}
+          dampingFactor={0.4}
+          />
+              <Stats/>   
+          <Casa/>
+  
+       
+
         </Suspense>
+      
       </Canvas>
     </div>
   );
