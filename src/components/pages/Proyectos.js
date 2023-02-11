36be9e051dff6c6cc7ad81c8249d1125a0517f0b
Domotiz Arq <davidchicaisa@gmail.com>
2023-02-11 17:36:32 -0500

    agregado texto flotante en three js

--- src/components/pages/Proyectos.js
+++ src/components/pages/Proyectos.js
@@ -1,7 +1,7 @@
 import React, { Component, Suspense } from "react";
 import Casa from "../casa/Casa";
 import CasaDavid from "../casa/CasaDavid";
-import { OrbitControls, Sky, Cloud } from "@react-three/drei";
+import { OrbitControls, Sky, Cloud, KeyboardControls} from "@react-three/drei";
 import { Canvas } from "react-three-fiber";
 import logo from "../../images/2devs.png";
 
@@ -27,39 +27,49 @@ export const OurAim = () => {
         display: "block",
       }}
     >
-      <Canvas camera={{ zoom: 1.2, position: [20, 10, 20] }}>
-        <ambientLight intensity={0.8} />
-        <pointLight position={[0, 0, -100]} intensity={2} />
-        <pointLight position={[35, 35, -100]} intensity={2} />
-        <Sky
-          sunPosition={[0, 0, -10]}
-          inclination={0.6}
-          azimuth={0.1}
-          turbidity={10}
-          rayleigh={0.5}
-          distance={1000}
-        />
-      
-        <Suspense fallback={null}>
-          <OrbitControls
-            enableDamping={true}
-            enablePan={true}
-            dampingFactor={0.4}
+      <KeyboardControls
+        map={[
+          { name: "forward", keys: ["ArrowUp", "w", "W"] },
+          { name: "backward", keys: ["ArrowDown", "s", "S"] },
+          { name: "left", keys: ["ArrowLeft", "a", "A"] },
+          { name: "right", keys: ["ArrowRight", "d", "D"] },
+          { name: "jump", keys: ["Space"] },
+        ]}
+      >
+        <Canvas camera={{ zoom: 1.2, position: [20, 10, 20] }}>
+          <ambientLight intensity={0.8} />
+          <pointLight position={[0, 0, -100]} intensity={2} />
+          <pointLight position={[35, 35, -100]} intensity={2} />
+          <Sky
+            sunPosition={[10, -1, -100]}
+            inclination={0.6}
+            azimuth={0.1}
+            turbidity={10}
+            rayleigh={0.5}
+            distance={1000}
           />
-          <Cloud position={[-4, 30, -25]} speed={0.2} opacity={1} />
-          <Cloud position={[4, 30, -15]} speed={0.2} opacity={0.5} />
-          <Cloud position={[-4, 30, -10]} speed={0.2} opacity={1} />
-          <Cloud position={[4, 30, -5]} speed={0.2} opacity={0.5} />
-          <Cloud position={[4, 30, 0]} speed={0.2} opacity={0.75} />
-          <Cloud position={[-50, 30, -25]} speed={0.2} opacity={1} />
-          <Cloud position={[50, 30, -15]} speed={0.2} opacity={0.5} />
-          <Cloud position={[-50, 30, -10]} speed={0.2} opacity={1} />
-          <Cloud position={[50, 30, -5]} speed={0.2} opacity={0.5} />
-          <Cloud position={[50, 30, 0]} speed={0.2} opacity={0.75} />
-          <Casa rotation={[0, Math.PI * 1.25, 0]} />
-        </Suspense>
-      </Canvas>
 
+          <Suspense fallback={null}>
+            <OrbitControls
+              enableDamping={true}
+              enablePan={true}
+              dampingFactor={0.4}
+            />
+            <Cloud position={[-4, 30, -25]} speed={0.2} opacity={1} />
+            <Cloud position={[4, 30, -15]} speed={0.2} opacity={0.5} />
+            <Cloud position={[-4, 30, -10]} speed={0.2} opacity={1} />
+            <Cloud position={[4, 30, -5]} speed={0.2} opacity={0.5} />
+            <Cloud position={[4, 30, 0]} speed={0.2} opacity={0.75} />
+            <Cloud position={[-50, 30, -25]} speed={0.2} opacity={1} />
+            <Cloud position={[50, 30, -15]} speed={0.2} opacity={0.5} />
+            <Cloud position={[-50, 30, -10]} speed={0.2} opacity={1} />
+            <Cloud position={[50, 30, -5]} speed={0.2} opacity={0.5} />
+            <Cloud position={[50, 30, 0]} speed={0.2} opacity={0.75} />
+            <Casa rotation={[0, Math.PI * 1.25, 0]} position={[0, 0, 0]} />
+          </Suspense>
+      
+        </Canvas>
+      </KeyboardControls>
       <div className="textInfo">
         {/*grid para la barra de informacion*/}
         <Grid container spacing={1}>
@@ -98,11 +108,13 @@ export const OurVision = () => {
         <pointLight position={[-35, 35, 0]} intensity={0.8} />
 
         <Sky
-          distance={450000}
-          sunPosition={[0, 1, 0]}
-          inclination={0}
-          azimuth={0.25}
-        />
+            sunPosition={[10, -1, -100]}
+            inclination={0.6}
+            azimuth={0.1}
+            turbidity={10}
+            rayleigh={0.5}
+            distance={1000}
+          />
 
         <Suspense fallback={null}>
           <OrbitControls
@@ -110,7 +122,16 @@ export const OurVision = () => {
             enablePan={true}
             dampingFactor={0.4}
           />
-
+     <Cloud position={[-4, 30, -25]} speed={0.2} opacity={1} />
+            <Cloud position={[4, 30, -15]} speed={0.2} opacity={0.5} />
+            <Cloud position={[-4, 30, -10]} speed={0.2} opacity={1} />
+            <Cloud position={[4, 30, -5]} speed={0.2} opacity={0.5} />
+            <Cloud position={[4, 30, 0]} speed={0.2} opacity={0.75} />
+            <Cloud position={[-50, 30, -25]} speed={0.2} opacity={1} />
+            <Cloud position={[50, 30, -15]} speed={0.2} opacity={0.5} />
+            <Cloud position={[-50, 30, -10]} speed={0.2} opacity={1} />
+            <Cloud position={[50, 30, -5]} speed={0.2} opacity={0.5} />
+            <Cloud position={[50, 30, 0]} speed={0.2} opacity={0.75} />
           <CasaDavid rotation={[0, Math.PI * 1.25, 0]} />
         </Suspense>
       </Canvas>
